e32d8f6566009d1725b350ac15b45073cf41b058
Nguyen-Van-Gia-Binh <nguyenvangiabinh22072006@gmail.com>
2026-02-23 04:35:53 +0700

    Upload src code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // App.js
-import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
 const App = () => {
   return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
+
+    <div className="app">
+      <h1 className="app-heading">E-Commerce Application</h1>
+      <div className="app-content">
         <ProductList />
         <ShoppingCart />
       </div>
+    </div>
+
 
   );
 };
